6eae670ceba213b68a019f3e2fba2169bf957449
Jeremy King <kingjr3@gmail.com>
2026-05-09 09:10:26 -0400

    Phase 13: Content: Sections E + F + G

--- go-gemini/src/content/lessons.ts
+++ go-gemini/src/content/lessons.ts
@@ -3,12 +3,18 @@ import { sectionALessons } from './lessons/section-a'
 import { sectionBLessons } from './lessons/section-b'
 import { sectionCLessons } from './lessons/section-c'
 import { sectionDLessons } from './lessons/section-d'
+import { sectionELessons } from './lessons/section-e'
+import { sectionFLessons } from './lessons/section-f'
+import { sectionGLessons } from './lessons/section-g'
 
 export const lessons: Lesson[] = [
   ...sectionALessons,
   ...sectionBLessons,
   ...sectionCLessons,
   ...sectionDLessons,
+  ...sectionELessons,
+  ...sectionFLessons,
+  ...sectionGLessons,
 ]
 
 export function getLessonBySlug(slug: string): Lesson | undefined {
